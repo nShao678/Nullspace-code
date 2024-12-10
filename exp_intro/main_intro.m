30808d15bc505079847ab573889aa11d620367dc
nShao678 <154589115+nShao678@users.noreply.github.com>
2024-12-10 15:03:35 +0100

    Update main_intro.m

--- exp_intro/main_intro.m
+++ exp_intro/main_intro.m
@@ -1,15 +1,16 @@
 clear all
-m = 21;
-n = 420;
-A = [zeros(1,m),(1:n-m)]';
+rng(1)
+m = 64;
+n = 10000+m;
+A = [zeros(1,m),1+(1:n-m)/n]';
 X = randn(n,m);
-blk = [1,2,3,4,5,6,7,10,12,14,15,20,21];
+blk = [1,2,4,8,16,32,64];
 len = length(blk);
 nz = zeros(1,len);
-iterMax = n;
+iterMax = 1024;
 for ii = 1:len
     X0 = orth(X(:,1:blk(ii)));
-    [~,~,nz(ii)] = Blanczos(@(x) A.*x,X0,iterMax);
+    [T,Q,nz(ii)] = Blanczos(@(x) A.*x,X0,iterMax);
 end
 latex(sym([blk;nz]))
 
